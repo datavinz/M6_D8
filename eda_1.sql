1bd38aa94b6d27e5118ecc52a351f2d536c2e761
Vincenzo Supino <119533580+datavinz@users.noreply.github.com>
2023-05-29 17:51:02 +0200

    Add files via upload

---
+++ eda_1.sql
@@ -0,0 +1,101 @@
+﻿CREATE TABLE Eda_1 AS
+(
+SELECT * FROM
+(SELECT 
+	(SELECT DISTINCT prodotto FROM fuel_price_it WHERE prodotto = "GPL") AS fuel, -- nome carburante
+	(SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2003 AND 2007) AND prodotto = "GPL") AS 03_07_avg, -- media prezzo 5 anni 03/07
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2008 AND 2012) AND prodotto = "GPL") AS 08_12_avg, -- media prezzo 5 anni 08/12
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2013 AND 2017) AND prodotto = "GPL") AS 13_17_avg, -- media prezzo 5 anni 13/17
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2018 AND 2022) AND prodotto = "GPL") AS 18_22_avg, -- media prezzo 5 anni 18/22
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "GPL" AND anno = 2022) AS 22_avg, -- media prezzo 2022, ultimo anno intero
+  (SELECT TRUNCATE(MIN(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "GPL") AS min_p, -- prezzo minimo
+  (SELECT DISTINCT anno FROM fuel_price_it 
+   	WHERE prodotto = "GPL" AND prezzo = (SELECT MIN(prezzo)
+  	FROM fuel_price_it WHERE prodotto = 'GPL')) AS min_p_y, -- anno prezzo minimo
+  (SELECT TRUNCATE(MAX(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "GPL") AS max_p, -- prezzo massimo
+  (SELECT DISTINCT anno FROM	fuel_price_it 
+   	WHERE prodotto = "GPL" AND prezzo = (SELECT MAX(prezzo)
+  	FROM fuel_price_it WHERE prodotto = 'GPL')) AS max_p_y, -- anno prezzo massimo
+	(SELECT TRUNCATE(AVG(tax), 3) FROM fuel_price_it
+   	WHERE prodotto = "GPL") AS avg_tax, -- media tasse
+  (SELECT TRUNCATE(AVG(inc_tax),2) FROM fuel_price_it WHERE prodotto = "GPL") AS avg_it, -- media incidenza tasse
+ 	(SELECT TRUNCATE(AVG(inc_tax),2) FROM fuel_price_it WHERE prodotto = "GPL" AND ANNO = 2022) AS avg_it22, -- media incidenza tasse 2022
+  (SELECT MAX(inc_tax) FROM fuel_price_it
+   	WHERE prodotto = "GPL") AS max_it, -- massimo incidenza tasse
+   (SELECT MIN(inc_tax) FROM fuel_price_it
+   	WHERE prodotto = "GPL") AS min_it) AS tb1 -- minimo incidenza tasse
+UNION 
+SELECT * FROM
+(SELECT 
+	(SELECT DISTINCT prodotto FROM fuel_price_it WHERE prodotto = "Benzina") AS fuel, -- nome carburante
+	(SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2003 AND 2007) AND prodotto = "Benzina") AS 03_07_avg, -- media prezzo 5 anni 03/07
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2008 AND 2012) AND prodotto = "Benzina") AS 08_12_avg, -- media prezzo 5 anni 08/12
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2013 AND 2017) AND prodotto = "Benzina") AS 13_17_avg, -- media prezzo 5 anni 13/17
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2018 AND 2022) AND prodotto = "Benzina") AS 18_22_avg, -- media prezzo 5 anni 18/22
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "Benzina" AND anno = 2022) AS 22_avg, -- media prezzo 2022, ultimo anno intero
+  (SELECT TRUNCATE(MIN(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "Benzina") AS min_p, -- prezzo minimo
+  (SELECT DISTINCT anno FROM fuel_price_it 
+   	WHERE prodotto = "Benzina" AND prezzo = (SELECT MIN(prezzo)
+  	FROM fuel_price_it WHERE prodotto = 'Benzina')) AS min_p_y, -- anno prezzo minimo
+  (SELECT TRUNCATE(MAX(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "Benzina") AS max_p, -- prezzo massimo
+  (SELECT DISTINCT anno FROM	fuel_price_it 
+   	WHERE prodotto = "Benzina" AND prezzo = (SELECT MAX(prezzo)
+  	FROM fuel_price_it WHERE prodotto = 'Benzina')) AS max_p_y, -- anno prezzo massimo
+	(SELECT TRUNCATE(AVG(tax), 3) FROM fuel_price_it
+   	WHERE prodotto = "Benzina") AS avg_tax, -- media tasse
+  (SELECT TRUNCATE(AVG(inc_tax),2) FROM fuel_price_it WHERE prodotto = "Benzina") AS avg_it, -- media incidenza tasse
+ 	(SELECT TRUNCATE(AVG(inc_tax),2) FROM fuel_price_it WHERE prodotto = "Benzina" AND ANNO = 2022) AS avg_it22, -- media incidenza tasse 2022
+  (SELECT MAX(inc_tax) FROM fuel_price_it
+   	WHERE prodotto = "Benzina") AS max_it, -- massimo incidenza tasse
+   (SELECT MIN(inc_tax) FROM fuel_price_it
+   	WHERE prodotto = "Benzina") AS min_it) AS tb2 -- minimo incidenza tasse
+UNION 
+SELECT * FROM
+(SELECT 
+  (SELECT DISTINCT prodotto FROM fuel_price_it WHERE prodotto = "Diesel") AS fuel, -- nome carburante
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2003 AND 2007) AND prodotto = "Diesel") AS 03_07_avg, -- media prezzo 5 anni 03/07
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2008 AND 2012) AND prodotto = "Diesel") AS 08_12_avg, -- media prezzo 5 anni 08/12
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2013 AND 2017) AND prodotto = "Diesel") AS 13_17_avg, -- media prezzo 5 anni 13/17
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE (anno BETWEEN 2018 AND 2022) AND prodotto = "Diesel") AS 18_22_avg, -- media prezzo 5 anni 18/22
+  (SELECT TRUNCATE(AVG(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "Diesel" AND anno = 2022) AS 22_avg, -- media prezzo 2022, ultimo anno intero
+  (SELECT TRUNCATE(MIN(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "Diesel") AS min_p, -- prezzo minimo
+  (SELECT DISTINCT anno FROM fuel_price_it 
+   	WHERE prodotto = "Diesel" AND prezzo = (SELECT MIN(prezzo)
+  	FROM fuel_price_it WHERE prodotto = 'Diesel')) AS min_p_y, -- anno prezzo minimo
+  (SELECT TRUNCATE(MAX(prezzo),3) FROM fuel_price_it 
+   	WHERE prodotto = "Diesel") AS max_p, -- prezzo massimo
+  (SELECT DISTINCT anno FROM	fuel_price_it 
+   	WHERE prodotto = "Diesel" AND prezzo = (SELECT MAX(prezzo)
+  	FROM fuel_price_it WHERE prodotto = 'Diesel')) AS max_p_y, -- anno prezzo massimo
+  (SELECT TRUNCATE(AVG(tax), 3) FROM fuel_price_it
+   	WHERE prodotto = "Diesel") AS avg_tax, -- media tasse
+  (SELECT TRUNCATE(AVG(inc_tax),2) FROM fuel_price_it WHERE prodotto = "Diesel") AS avg_it, -- media incidenza tasse
+ 	(SELECT TRUNCATE(AVG(inc_tax),2) FROM fuel_price_it WHERE prodotto = "Diesel" AND ANNO = 2022) AS avg_it22, -- media inc tax22
+  (SELECT MAX(inc_tax) FROM fuel_price_it
+   	WHERE prodotto = "Diesel") AS max_it, -- massimo incidenza tasse
+  (SELECT MIN(inc_tax) FROM fuel_price_it
+   	WHERE prodotto = "Diesel") AS min_it) AS tb3 -- minimo incidenza tasse
+ ); 
+
+
+
---
+++ eda_car.sql
@@ -0,0 +1,72 @@
+﻿CREATE TABLE eda_2 AS
+(
+SELECT * FROM
+ 	(SELECT
+   	(SELECT DISTINCT prodotto FROM fuel_price_it WHERE prodotto = "Diesel") AS fuel,
+  	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * (SELECT TRUNCATE((655/(100/4.3)),3)) 
+      FROM fuel_price_it 
+      WHERE prodotto = "Diesel" AND anno = 2003) AS rom_mil_car_03,
+   	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * (SELECT TRUNCATE((655/(100/4.3)),3)) 
+      FROM fuel_price_it 
+      WHERE prodotto = "Diesel" AND anno = 2022) AS rom_mil_car_22, 
+   	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * 4.3 
+      FROM fuel_price_it 
+      WHERE prodotto = "Diesel" AND anno = 2003) AS 100km_car_03,
+   (SELECT 
+    	TRUNCATE(AVG(prezzo),3) * 4.3
+      FROM fuel_price_it 
+      WHERE prodotto = "Diesel" AND anno = 2022) AS 100km_car_22) AS tb4
+UNION
+SELECT * FROM
+ (SELECT
+   	(SELECT DISTINCT prodotto FROM fuel_price_it WHERE prodotto = "Benzina") AS fuel,
+  	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * (SELECT TRUNCATE((655/(100/5)),3)) 
+      FROM fuel_price_it 
+      WHERE prodotto = "Benzina" AND anno = 2003) AS rom_mil_car_03,
+   	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * (SELECT TRUNCATE((655/(100/5)),3)) 
+      FROM fuel_price_it 
+      WHERE prodotto = "Benzina" AND anno = 2022) AS rom_mil_car_22, 
+   	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * 5
+      FROM fuel_price_it 
+      WHERE prodotto = "Benzina" AND anno = 2003) AS 100km_car_03,
+   (SELECT 
+    	TRUNCATE(AVG(prezzo),3) * 5
+      FROM fuel_price_it 
+      WHERE prodotto = "Benzina" AND anno = 2022) AS 100km_car_22) AS tb5
+UNION
+SELECT * FROM
+ (SELECT
+   	(SELECT DISTINCT prodotto FROM fuel_price_it WHERE prodotto = "GPL") AS fuel,
+  	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * (SELECT TRUNCATE((655/(100/5)),3)) 
+      FROM fuel_price_it 
+      WHERE prodotto = "GPL" AND anno = 2003) AS rom_mil_car_03,
+   	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * (SELECT TRUNCATE((655/(100/5)),3)) 
+      FROM fuel_price_it 
+      WHERE prodotto = "GPL" AND anno = 2022) AS rom_mil_car_22, 
+   	(SELECT 
+    	TRUNCATE(AVG(prezzo),3) * 7.2
+      FROM fuel_price_it 
+      WHERE prodotto = "GPL" AND anno = 2003) AS 100km_car_03,
+   (SELECT 
+    	TRUNCATE(AVG(prezzo),3) * 7.2
+      FROM fuel_price_it 
+      WHERE prodotto = "GPL" AND anno = 2022) AS 100km_car_22) AS tb6
+  );
+
+
+CREATE TABLE eda_car AS
+(SELECT 
+ fuel,
+ TRUNCATE(rom_mil_car_03,2) AS rom_mil_car_03,
+ TRUNCATE(rom_mil_car_22,2) AS rom_mil_car_22,
+ TRUNCATE(100km_car_03,2) AS 100km_car_03,
+ TRUNCATE(100km_car_22,2) AS 100km_car_22
+FROM eda_2);
